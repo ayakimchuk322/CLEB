e5a2138367bce7c4ff3b8e1d028bcbd58d66ff08
Tol182 <toldark182@gmail.com>
2016-08-04 17:12:53 +0300

    basic uploading feature

--- src/main/java/cleb/Uploader.java
+++ src/main/java/cleb/Uploader.java
@@ -1,7 +1,6 @@
 package cleb;
 
 import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
@@ -16,75 +15,89 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class UploadServlet
+ * This servlet handles uploading user books onto server.
  */
 public class Uploader extends HttpServlet {
+    //  UID To satisfy compiler
     private static final long serialVersionUID = 1L;
 
-    private static final String DATA_DIRECTORY = "data";
-    private static final int MAX_MEMORY_SIZE = 1024 * 1024 * 2;
-    private static final int MAX_REQUEST_SIZE = 1024 * 1024;
+    private boolean isMultipart;
+    private String filePath;
+    // Max book size to be uploaded (10MB)
+    private int maxFileSize = 10 * 1024 * 1024;
+    private File file;
 
     @Override
-    protected void doPost(HttpServletRequest request,
-	    HttpServletResponse response) throws ServletException, IOException {
+    public void init() {
+	// Get the file location where it would be stored.
+	filePath = getServletContext().getInitParameter("file-upload");
+    }
 
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
 	// Check that we have a file upload request
-	boolean isMultipart = ServletFileUpload.isMultipartContent(request);
-
+	isMultipart = ServletFileUpload.isMultipartContent(request);
+	response.setContentType("text/html");
+	java.io.PrintWriter out = response.getWriter();
 	if (!isMultipart) {
+	    out.println("<html>");
+	    out.println("<head>");
+	    out.println("<title>Servlet upload</title>");
+	    out.println("</head>");
+	    out.println("<body>");
+	    out.println("<p>No file uploaded</p>");
+	    out.println("</body>");
+	    out.println("</html>");
 	    return;
 	}
 
-	// Create a factory for disk-based file items
 	DiskFileItemFactory factory = new DiskFileItemFactory();
-
-	// Sets the size threshold beyond which files are written directly to
-	// disk.
-	factory.setSizeThreshold(MAX_MEMORY_SIZE);
-
-	// Sets the directory used to temporarily store files that are larger
-	// than the configured size threshold. We use temporary directory for
-	// java
-	factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
-
-	// constructs the folder where uploaded file will be stored
-	String uploadFolder = getServletContext().getRealPath("")
-		+ File.separator + DATA_DIRECTORY;
 
 	// Create a new file upload handler
 	ServletFileUpload upload = new ServletFileUpload(factory);
-
-	// Set overall request size constraint
-	upload.setSizeMax(MAX_REQUEST_SIZE);
+	// Maximum file size to be uploaded.
+	upload.setSizeMax(maxFileSize);
 
 	try {
-	    // Parse the request
-	    List items = upload.parseRequest(request);
-	    Iterator iter = items.iterator();
-	    while (iter.hasNext()) {
-		FileItem item = (FileItem) iter.next();
+	    // Parse the request to get file items.
+	    List<FileItem> fileItems = upload.parseRequest(request);
 
-		if (!item.isFormField()) {
-		    String fileName = new File(item.getName()).getName();
-		    String filePath = uploadFolder + File.separator + fileName;
-		    File uploadedFile = new File(filePath);
-		    System.out.println(filePath);
-		    // saves the file to upload directory
-		    item.write(uploadedFile);
+	    // Process the uploaded file items
+	    Iterator<FileItem> iterator = fileItems.iterator();
+
+	    out.println("<html>");
+	    out.println("<head>");
+	    out.println("<title>Servlet upload</title>");
+	    out.println("</head>");
+	    out.println("<body>");
+
+	    while (iterator.hasNext()) {
+		FileItem fi = iterator.next();
+		if (!fi.isFormField()) {
+		    // Get the uploaded file parameters
+		    String fieldName = fi.getFieldName();
+		    String fileName = fi.getName();
+		    String contentType = fi.getContentType();
+		    boolean isInMemory = fi.isInMemory();
+		    long sizeInBytes = fi.getSize();
+		    // Write the file
+		    if (fileName.lastIndexOf("\\") >= 0) {
+			file = new File(filePath + fileName
+				.substring(fileName.lastIndexOf("\\")));
+		    } else {
+			file = new File(filePath + fileName
+				.substring(fileName.lastIndexOf("\\") + 1));
+		    }
+		    fi.write(file);
+		    out.println("Uploaded Filename: " + fileName + "<br>");
 		}
 	    }
-
-	    // displays done.jsp page after upload finished
-	    getServletContext().getRequestDispatcher("/done.jsp")
-	    .forward(request, response);
-
-	} catch (FileUploadException ex) {
-	    throw new ServletException(ex);
+	    out.println("</body>");
+	    out.println("</html>");
 	} catch (Exception ex) {
-	    throw new ServletException(ex);
+	    System.out.println(ex);
 	}
-
     }
 
 }

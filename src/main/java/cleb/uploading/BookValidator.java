cbeb41ebd0ee91367037f6f09899da74a4d71b72
Tol182 <toldark182@gmail.com>
2016-08-06 20:48:19 +0300

    added basic xml validation logic

--- src/main/java/cleb/uploading/BookValidator.java
+++ src/main/java/cleb/uploading/BookValidator.java
@@ -1,5 +1,9 @@
 package cleb.uploading;
 
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+import java.io.File;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -10,8 +14,19 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * This servlet validates new books against XSD.
  */
-public class Validator extends HttpServlet {
+// TODO remove e.prinstacktraces
+public class BookValidator extends HttpServlet {
     private static final long serialVersionUID = 1L;
+
+    private String tempFolderPath;
+
+    @Override
+    public void init() throws ServletException {
+	// Directory for temporary storing uploaded books - till it's checked by
+	// this servlet
+	tempFolderPath = getServletContext()
+		.getInitParameter("file-temp-upload");
+    }
 
     @Override
     protected void doGet(HttpServletRequest request,
@@ -22,7 +37,28 @@ public class Validator extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request,
 	    HttpServletResponse response) throws ServletException, IOException {
+	String tempBookPath = tempFolderPath + request.getParameter("book");
+	File tempBookFile = new File(tempBookPath);
 
+	validateBook(tempBookFile);
+    }
+
+    private boolean validateBook(File file) {
+	boolean validated = false;
+
+	SAXBuilder builder = new SAXBuilder();
+	try {
+	    builder.build(file);
+
+	    validated = true;
+	} catch (JDOMException e) {
+	    e.printStackTrace();
+	    validated = false;
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+
+	return validated;
     }
 
 }

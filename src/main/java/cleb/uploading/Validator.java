235c581641d630b5b16b7f913b2ef4a3e4ac32af
Tol182 <toldark182@gmail.com>
2016-08-06 18:03:08 +0300

    added new servlet to validate uploaded books against XSD

---
+++ src/main/java/cleb/uploading/Validator.java
@@ -0,0 +1,28 @@
+package cleb.uploading;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * This servlet validates new books against XSD.
+ */
+public class Validator extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+	    HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+	    HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}

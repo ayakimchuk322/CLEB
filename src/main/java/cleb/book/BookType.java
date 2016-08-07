f3e3db9a045d2f0db75cbd1934a351eb0f07fd7d
Tol182 <toldark182@gmail.com>
2016-08-07 17:05:58 +0300

    added enum with supported book types

---
+++ src/main/java/cleb/book/BookType.java
@@ -0,0 +1,18 @@
+package cleb.book;
+
+/**
+ * Enumeration of all supported e-book types by this library.
+ */
+public enum BookType {
+    FB2("fb2");
+
+    private final String type;
+
+    private BookType(String type) {
+	this.type = type;
+    }
+
+    public String type() {
+	return this.type;
+    }
+}

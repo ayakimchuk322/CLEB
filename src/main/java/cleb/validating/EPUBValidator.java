48482d155de69b8784e5a8120356d50e36be4ec8
Tol182 <toldark182@gmail.com>
2016-08-12 22:18:40 +0300

    added epub validation logic

--- src/main/java/cleb/validating/EPUBValidator.java
+++ src/main/java/cleb/validating/EPUBValidator.java
@@ -9,17 +9,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+
 /**
  * This servlet validates new epub books.
  */
+// TODO remove e.printstacktraces
 public class EPUBValidator extends HttpServlet implements IValidator {
     private static final long serialVersionUID = 1L;
 
     private String tempFolderPath;
 
-    // XXX zip object?
-    // TODO rename attributes passing by servlets (esp book)
-    private Object document;
+    private ZipFile book;
 
     /**
      * Initializes temporary directory.
@@ -40,7 +42,7 @@ public class EPUBValidator extends HttpServlet implements IValidator {
         File tempBookFile = new File(tempBookPath);
 
         if (validateBook(tempBookFile)) {
-            request.setAttribute("book", document);
+            request.setAttribute("book", book);
             RequestDispatcher dispatcher = request
                     .getRequestDispatcher("/EPUBSaver");
             dispatcher.forward(request, response);
@@ -61,6 +63,24 @@ public class EPUBValidator extends HttpServlet implements IValidator {
     @Override
     public boolean validateBook(File file) {
         boolean validated = false;
+
+        try {
+            book = new ZipFile(file);
+
+            if (book.isValidZipFile()) {
+                try {
+                    // Any valid epub book should contain these two files
+                    book.getFileHeader("META-INF/container.xml").getFileName();
+                    book.getFileHeader("mimetype").getFileName();
+                } catch (NullPointerException e) {
+                    validated = false;
+                }
+            } else {
+                validated = false;
+            }
+        } catch (ZipException e) {
+            e.printStackTrace();
+        }
 
         return validated;
     }

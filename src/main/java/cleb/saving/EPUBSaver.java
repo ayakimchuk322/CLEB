4fe945df17b8f06622b291691b316a9d6f075f92
Tol182 <toldark182@gmail.com>
2016-08-13 12:16:55 +0300

    added logic to extract data about epub book

--- src/main/java/cleb/saving/EPUBSaver.java
+++ src/main/java/cleb/saving/EPUBSaver.java
@@ -1,8 +1,16 @@
 package cleb.saving;
 
+import org.apache.commons.io.FileUtils;
 import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.Namespace;
+import org.jdom2.input.SAXBuilder;
 
+import java.io.File;
 import java.io.IOException;
+import java.time.Instant;
+import java.time.temporal.ChronoField;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
 
 /**
  * This servlet stores basic information about epub book in database and places
@@ -24,7 +33,7 @@ public class EPUBSaver extends HttpServlet implements ISaver {
 
     private String folderPath;
 
-    private Document document;
+    private Object book;
 
     /**
      * Initializes temporary and storing directories.
@@ -45,9 +54,9 @@ public class EPUBSaver extends HttpServlet implements ISaver {
                 + (String) request.getAttribute("file");
         String bookPath = folderPath + (String) request.getAttribute("file");
 
-        document = (Document) request.getAttribute("book");
+        book = request.getAttribute("book");
 
-        if (getBasicInfo(request, document)) {
+        if (getBasicInfo(request, book)) {
             storeInDir(tempBookPath, bookPath);
         } else {
             // TODO show user error page
@@ -78,9 +87,86 @@ public class EPUBSaver extends HttpServlet implements ISaver {
         // Necessary cast to process with book
         ZipFile zip = (ZipFile) book;
 
+        // Add random suffix to temporary directory based on current time
+        String suffix = String
+                .valueOf(Instant.now().get(ChronoField.MILLI_OF_SECOND));
+
+        // Extract META-INF/container.xml and get the location of content.opf
+        // It's location inside book differs from book to book
+        // content.opf has the neccessary information about the book
+        String containerxmlPath = tempFolderPath + "/" + suffix + "/";
+
+        try {
+            zip.extractFile("META-INF/container.xml", containerxmlPath);
+
+            SAXBuilder builder = new SAXBuilder();
+
+            Document containerxmlDoc = builder.build(
+                    new File(containerxmlPath + "/META-INF/container.xml"));
+
+            // container.xml root and namespace
+            Element containerxmlRoot = containerxmlDoc.getRootElement();
+            Namespace containerxmlNs = containerxmlRoot.getNamespace();
+
+            // content.opf location inside epub book
+            String contentopfPath = containerxmlRoot
+                    .getChild("rootfiles", containerxmlNs)
+                    .getChild("rootfile", containerxmlNs)
+                    .getAttributeValue("full-path");
+
+            zip.extractFile(contentopfPath, containerxmlPath);
+
+            Document contentopfDoc = builder
+                    .build(new File(containerxmlPath + contentopfPath));
+
+            // content.opf root and namespace
+            Element contentopfRoot = contentopfDoc.getRootElement();
+            Namespace contentopfNs = contentopfRoot.getNamespace();
+            Namespace dc = contentopfRoot.getNamespace("dc");
+
+            // Get required elements to extract necessary information about book
+            Element metadataEl = null;
+            Element titleEl = null;
+            Element creatorEl = null;
+
+            // Not all books have all these items, that's why every one
+            // statement below covered in try catch block
+            try {
+                metadataEl = contentopfRoot.getChild("metadata", contentopfNs);
+            } catch (NullPointerException e) {
+            }
+
+            try {
+                titleEl = metadataEl.getChild("title", dc);
+                title = titleEl.getText();
+            } catch (NullPointerException e) {
+            }
+
+            try {
+                creatorEl = metadataEl.getChild("creator", dc);
+                authorFirstName = creatorEl.getText();
+                authorLastName = creatorEl.getText();
+            } catch (NullPointerException e) {
+            }
+
+        } catch (ZipException | JDOMException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            cleanTmpDir(containerxmlPath);
+        }
+
         return storeInDB(fileName, md5, fileSize, fileType, genre,
                 authorFirstName, authorLastName, title, seqName, seqNumber,
                 published, uploadedBy);
     }
 
+    /**
+     * This method deletes temporarily extracted files from epub book.
+     *
+     * @param path
+     *        Directory to delete
+     */
+    private void cleanTmpDir(String path) {
+        FileUtils.deleteQuietly(new File(path));
+    }
 }

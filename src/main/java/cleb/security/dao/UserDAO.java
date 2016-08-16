fd1f296a7945fb4fa95370f912b42ee0052f2131
Tol182 <toldark182@gmail.com>
2016-08-16 13:19:02 +0300

    added DAO classes to manage shiro users & roles

---
+++ src/main/java/cleb/security/dao/UserDAO.java
@@ -0,0 +1,34 @@
+package cleb.security.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import cleb.security.tables.User;
+
+// TODO add javadoc
+public class UserDAO {
+
+    @SuppressWarnings("deprecation")
+    public static User getUserByEmail(String email) {
+        User user = null;
+
+        SessionFactory factory = new Configuration().configure()
+            .buildSessionFactory();
+
+        Transaction transaction = null;
+
+        try (Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            user = (User) session.createQuery("from users where email=?")
+                .setParameter(0, email).uniqueResult();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+
+}
---
+++ src/main/java/cleb/security/dao/UserRoleDAO.java
@@ -0,0 +1,55 @@
+package cleb.security.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+import cleb.security.tables.UserRole;
+
+// TODO add javadoc
+public class UserRoleDAO {
+
+    @SuppressWarnings({ "deprecation", "unchecked" })
+    public static List<UserRole> getUserRolesByEmail(String email) {
+        List<UserRole> roles = null;
+
+        SessionFactory factory = new Configuration().configure()
+            .buildSessionFactory();
+
+        Transaction transaction = null;
+
+        try (Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            roles = session.createQuery("from userroles where email=?")
+                .setParameter(0, email).list();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return roles;
+    }
+
+    public static void insert(UserRole role) {
+        SessionFactory factory = new Configuration().configure()
+            .buildSessionFactory();
+
+        Transaction transaction = null;
+
+        try (Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            session.save(role);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+
+            e.printStackTrace();
+        }
+    }
+
+}

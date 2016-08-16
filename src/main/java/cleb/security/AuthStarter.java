0ea63b56f50763e259d52a2310035e27b4f81bb0
Tol182 <toldark182@gmail.com>
2016-08-16 15:08:05 +0300

    added servlet for initializing shiro
    it auto starts with server

---
+++ src/main/java/cleb/security/AuthStarter.java
@@ -0,0 +1,28 @@
+package cleb.security;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.util.Factory;
+import org.apache.shiro.mgt.SecurityManager;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class AuthStarter
+ */
+public class AuthStarter extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory();
+
+        SecurityManager securityManager = factory.getInstance();
+
+        SecurityUtils.setSecurityManager(securityManager);
+    }
+
+}

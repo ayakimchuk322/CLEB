f29d51f02400b4f8742ac98e38fd6eb7565a28ae
Tol182 <toldark182@gmail.com>
2016-08-16 15:07:23 +0300

    added custom realm for shiro

---
+++ src/main/java/cleb/security/ClebRealm.java
@@ -0,0 +1,40 @@
+package cleb.security;
+
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SaltedAuthenticationInfo;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.realm.jdbc.JdbcRealm;
+
+import cleb.security.dao.UserDAO;
+import cleb.security.tables.User;
+
+public class ClebRealm extends JdbcRealm {
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(
+        AuthenticationToken token) {
+        // Identify account to log to
+        UsernamePasswordToken userPassToken = (UsernamePasswordToken) token;
+        final String username = userPassToken.getUsername();
+
+        if (username == null) {
+            System.out.println("Username is null.");
+            return null;
+        }
+
+        // Read password hash and salt from db
+        final User user = UserDAO.getUserByEmail(username);
+
+        if (user == null) {
+            System.out.println("No account found for user [" + username + "]");
+            return null;
+        }
+
+        // Return salted credentials
+        SaltedAuthenticationInfo info = new ClebSaltedAuthenticationInfo(
+            username, user.getPassword(), user.getSalt());
+
+        return info;
+    }
+}

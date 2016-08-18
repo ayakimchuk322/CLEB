ab02607c4214b23ae479217b86cd02f3c79fb6dc
Tol182 <toldark182@gmail.com>
2016-08-18 12:23:37 +0300

    moved registering logic from servlet to DAO class

--- src/main/java/cleb/security/dao/UserDAO.java
+++ src/main/java/cleb/security/dao/UserDAO.java
@@ -1,11 +1,15 @@
 package cleb.security.dao;
 
+import org.apache.shiro.crypto.RandomNumberGenerator;
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.Sha256Hash;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import cleb.security.tables.User;
+import cleb.security.tables.UserRole;
 
 // TODO add javadoc
 public class UserDAO {
@@ -29,6 +33,58 @@ public class UserDAO {
         }
 
         return user;
+    }
+
+    public static void registrate(String name, String email,
+        String plainTextPassword) {
+        // Create new user
+        User user = new User();
+        user.setUserName(name);
+        user.setEmail(email);
+
+        generatePassword(user, plainTextPassword);
+
+        // Create 'reader' role for new user
+        UserRole role = new UserRole();
+        role.setEmail(email);
+        role.setRoleName("reader");
+
+        // Proceed with db
+        SessionFactory factory = new Configuration().configure()
+            .buildSessionFactory();
+
+        Transaction transaction = null;
+
+        try (Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            // Save user in db
+            session.save(user);
+
+            // Save role in db
+            session.save(role);
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+
+            e.printStackTrace();
+        }
+    }
+
+    private static void generatePassword(User user, String plainTextPassword) {
+        RandomNumberGenerator rng = new SecureRandomNumberGenerator();
+        Object salt = rng.nextBytes();
+
+        // Hash the plain-text password with the random salt and multiple
+        // iterations and then Base64-encode the value
+        String hashedPasswordBase64 = new Sha256Hash(plainTextPassword, salt,
+            1024).toBase64();
+
+        // Update user object password and salt
+        user.setPassword(hashedPasswordBase64);
+        user.setSalt(salt.toString());
     }
 
 }
--- src/main/java/cleb/security/servlets/RegisterServlet.java
+++ src/main/java/cleb/security/servlets/RegisterServlet.java
@@ -1,12 +1,6 @@
 package cleb.security.servlets;
 
-import org.apache.shiro.crypto.RandomNumberGenerator;
-import org.apache.shiro.crypto.SecureRandomNumberGenerator;
-import org.apache.shiro.crypto.hash.Sha256Hash;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
+import static cleb.security.dao.UserDAO.registrate;
 
 import java.io.IOException;
 
@@ -15,9 +9,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import cleb.security.tables.User;
-import cleb.security.tables.UserRole;
 
 // TODO add javadoc
 // FIXME add check before registering if not registered already
@@ -51,69 +42,17 @@ public class RegisterServlet extends HttpServlet {
                 "Wrong parameters: fill all required fields.");
         } else {
             // Proceed with saving in db
-            SessionFactory factory = new Configuration().configure()
-                .buildSessionFactory();
+            registrate(name, email, password);
 
-            Transaction transaction = null;
-
-            try (Session session = factory.openSession()) {
-                transaction = session.beginTransaction();
-
-                registrate(session, name, email, password);
-
-                // Inform user about successfull registering
-                request.setAttribute("message", "User created. "
-                    + "You can now <a href='login.jsp'>login</a>.");
-
-                transaction.commit();
-            } catch (Exception e) {
-                if (transaction != null) {
-                    transaction.rollback();
-                }
-
-                e.printStackTrace();
-            }
+            // Inform user about successfull registering
+            request.setAttribute("message", "User created. "
+                + "You can now <a href='login.jsp'>login</a>.");
         }
 
         // Show register.jsp page
         RequestDispatcher dispatcher = request
             .getRequestDispatcher("/register.jsp");
         dispatcher.include(request, response);
-    }
-
-    private void registrate(Session session, String name, String email,
-        String plainTextPassword) {
-        // Create new user
-        User user = new User();
-        user.setUserName(name);
-        user.setEmail(email);
-
-        generatePassword(user, plainTextPassword);
-
-        // Save user in db
-        session.save(user);
-
-        // Create 'reader' role
-        UserRole role = new UserRole();
-        role.setEmail(email);
-        role.setRoleName("reader");
-
-        // Save role in db
-        session.save(role);
-    }
-
-    private void generatePassword(User user, String plainTextPassword) {
-        RandomNumberGenerator rng = new SecureRandomNumberGenerator();
-        Object salt = rng.nextBytes();
-
-        // Hash the plain-text password with the random salt and multiple
-        // iterations and then Base64-encode the value
-        String hashedPasswordBase64 = new Sha256Hash(plainTextPassword, salt,
-            1024).toBase64();
-
-        // Update user object password and salt
-        user.setPassword(hashedPasswordBase64);
-        user.setSalt(salt.toString());
     }
 
 }

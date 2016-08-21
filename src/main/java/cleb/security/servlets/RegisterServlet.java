b9868ccb00f21633a890c07ca03ad20c18d093bf
Tol182 <toldark182@gmail.com>
2016-08-21 21:36:33 +0300

    removed unused messaging, added TODO task

--- src/main/java/cleb/security/servlets/RegisterServlet.java
+++ src/main/java/cleb/security/servlets/RegisterServlet.java
@@ -1,6 +1,6 @@
 package cleb.security.servlets;
 
-import static cleb.security.dao.UserDAO.registrate;
+import static cleb.security.dao.UserDAO.register;
 
 import java.io.IOException;
 
@@ -36,14 +36,10 @@ public class RegisterServlet extends HttpServlet {
         // Check for name, email and password being not null
         if (name == null || email == null || password == null) {
             // Inform user about wrong parameters
-            request.setAttribute("message",
-                "Wrong parameters: fill all required fields.");
         } else {
             // Proceed with saving in db
-            registrate(name, email, password);
-
-            // Inform user about successfull registering
-            request.setAttribute("message", "User created. You can now login.");
+            // TODO add if check
+            register(name, email, password);
         }
 
         // Show login.jsp page

836d6c3bd2ceea6f76ff1fde177cd3b2d9dc2087
Tol182 <toldark182@gmail.com>
2016-08-24 02:43:55 +0300

    added insert of a random quote in index page

--- src/main/java/cleb/reading/IndexServlet.java
+++ src/main/java/cleb/reading/IndexServlet.java
@@ -1,10 +1,17 @@
 package cleb.reading;
 
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Random;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -23,6 +30,10 @@ public class IndexServlet extends HttpServlet {
     private ServletContextTemplateResolver templateResolver;
     private TemplateEngine templateEngine;
 
+    private List<Element> quotesEl;
+
+    private Random random;
+
     @Override
     public void init() throws ServletException {
         // Initialize Thymeleaf for this servlet
@@ -34,15 +45,43 @@ public class IndexServlet extends HttpServlet {
         templateResolver.setSuffix(".html");
         templateEngine = new TemplateEngine();
         templateEngine.setTemplateResolver(templateResolver);
+
+        // Load quotes from xml file
+        InputStream quotesIn = getServletContext()
+            .getResourceAsStream("/WEB-INF/quotes.xml");
+
+        SAXBuilder builder = new SAXBuilder();
+        try {
+            Document quotesDoc = builder.build(quotesIn);
+            Element rootEl = quotesDoc.getRootElement();
+            quotesEl = rootEl.getChildren();
+
+        } catch (JDOMException e) {
+        } catch (IOException e) {
+        }
+
+        // Initialize random generator
+        random = new Random();
     }
 
     @Override
     protected void doGet(HttpServletRequest request,
         HttpServletResponse response) throws ServletException, IOException {
 
+        // Get random quote from list
+        int quotesSize = quotesEl.size();
+        Element quote = quotesEl.get(random.nextInt(quotesSize));
+
+        String quoteText = quote.getChildText("text");
+        String quoteAuthor = quote.getChildText("author");
+
         // Show index.html page
         WebContext webContext = new WebContext(request, response,
             servletContext, request.getLocale());
+
+        webContext.setVariable("quote", quoteText);
+        webContext.setVariable("author", quoteAuthor);
+
         templateEngine.process("index", webContext, response.getWriter());
     }
 

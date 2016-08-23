c0474ed78a9abf88a6aa2213db5888caeffb0359
Tol182 <toldark182@gmail.com>
2016-08-23 22:14:58 +0300

    added new servlet to handle errors

---
+++ src/main/java/cleb/error/ErrorServlet.java
@@ -0,0 +1,18 @@
+package cleb.error;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ErrorServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+        HttpServletResponse response) throws ServletException, IOException {
+    }
+
+}

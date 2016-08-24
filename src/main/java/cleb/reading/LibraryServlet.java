c4b237edcb6ce1fc38211a4318f2245592f367c1
Tol182 <toldark182@gmail.com>
2016-08-25 00:03:40 +0300

    added Thymeleaf support

--- src/main/java/cleb/reading/LibraryServlet.java
+++ src/main/java/cleb/reading/LibraryServlet.java
@@ -1,6 +1,12 @@
 package cleb.reading;
 
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
 import java.io.IOException;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,9 +17,32 @@ public class LibraryServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
 
+    private ServletContext servletContext;
+    private ServletContextTemplateResolver templateResolver;
+    private TemplateEngine templateEngine;
+
+    @Override
+    public void init() throws ServletException {
+        // Initialize Thymeleaf for this servlet
+        servletContext = getServletContext();
+        templateResolver = new ServletContextTemplateResolver(servletContext);
+        templateResolver.setTemplateMode("HTML5");
+        // Prefix and suffix for template
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+    }
+
     @Override
     protected void doGet(HttpServletRequest request,
         HttpServletResponse response) throws ServletException, IOException {
+
+        // Show library.html page
+        WebContext webContext = new WebContext(request, response,
+            servletContext, request.getLocale());
+
+        templateEngine.process("library", webContext, response.getWriter());
     }
 
     @Override

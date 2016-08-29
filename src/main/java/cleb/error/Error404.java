dea32a93e559c1c46655ba3269f07b6664572df0
Tol182 <toldark182@gmail.com>
2016-08-30 00:44:37 +0300

    added handling 404

---
+++ src/main/java/cleb/error/Error404.java
@@ -0,0 +1,73 @@
+package cleb.error;
+
+import static cleb.security.dao.UserDAO.getUserNameBySubject;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * This class serves standard 404.html page to users in case requested page is
+ * unavailable.
+ */
+public class Error404 extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    private ServletContext servletContext;
+    private ServletContextTemplateResolver templateResolver;
+    private TemplateEngine templateEngine;
+
+    @Override
+    public void init() throws ServletException {
+        // Initialize Thymeleaf for this servlet
+        servletContext = getServletContext();
+        templateResolver = new ServletContextTemplateResolver(servletContext);
+        templateResolver.setTemplateMode("HTML5");
+        // Prefix and suffix for template
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+        HttpServletResponse response) throws ServletException, IOException {
+
+        WebContext webContext = new WebContext(request, response,
+            servletContext, request.getLocale());
+
+        // Get current user
+        Subject currentUser = SecurityUtils.getSubject();
+
+        if (currentUser.isAuthenticated()) {
+            String userName = getUserNameBySubject(currentUser);
+            // Set username variable
+            webContext.setVariable("username", userName);
+        }
+
+        // Retrieve requested page
+        String requestedPage = (String) request
+            .getAttribute("javax.servlet.forward.request_uri");
+        // Get rid of context '/cleb/'
+        requestedPage = requestedPage.substring(6);
+
+        // Set variable for template
+        webContext.setVariable("requestedpage", requestedPage);
+
+        // Show 404.html page
+        templateEngine.process("404", webContext, response.getWriter());
+    }
+
+}

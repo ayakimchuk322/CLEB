60daa5426ad37a443a882005f9cc2c9a1975e326
Tol182 <toldark182@gmail.com>
2016-08-30 22:45:48 +0300

    added javadoc

--- src/main/java/cleb/library/reading/IReader.java
+++ src/main/java/cleb/library/reading/IReader.java
@@ -2,8 +2,21 @@ package cleb.library.reading;
 
 import java.io.File;
 
-// TODO add javadoc
+/**
+ * This interface contains method to get text from book and return it as a
+ * {@code String} to caller. Any concrete reader class should implement this
+ * interface and provide specific implementations for {@code read} method based
+ * on book type.
+ */
 public interface IReader {
 
+    /**
+     * Implementation of this method should parse {@code bookFile} book and
+     * return it's text as a {@code String}.
+     *
+     * @param bookFile {@code File} book to parse.
+     *
+     * @return {@code String} book text.
+     */
     public String read(File bookFile);
 }

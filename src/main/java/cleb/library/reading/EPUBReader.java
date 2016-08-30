d0ea6204a4a6a44e09ffc6b17e11638775b1a9b0
Tol182 <toldark182@gmail.com>
2016-08-31 01:29:44 +0300

    minor refactoring, added javadoc

--- src/main/java/cleb/library/reading/EPUBReader.java
+++ src/main/java/cleb/library/reading/EPUBReader.java
@@ -2,14 +2,25 @@ package cleb.library.reading;
 
 import static cleb.security.dao.UserDAO.getUserNameBySubject;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.Namespace;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.XMLOutputter;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.List;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -19,12 +30,18 @@ import javax.servlet.http.HttpServletResponse;
 
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
+import net.lingala.zip4j.model.FileHeader;
 
-// TODO add javadoc
-// TODO replace printstacktrace
+/**
+ * This class reads epub books.
+ */
 public class EPUBReader extends HttpServlet implements IReader {
 
     private static final long serialVersionUID = 1L;
+
+    // Logger for this class
+    private static final Logger logger = LogManager
+        .getLogger(EPUBReader.class.getName());
 
     private ServletContext servletContext;
     private ServletContextTemplateResolver templateResolver;
@@ -87,18 +104,86 @@ public class EPUBReader extends HttpServlet implements IReader {
     }
 
     @Override
+    @SuppressWarnings("rawtypes")
     public String read(File bookFile) {
         String bookText = "";
 
+        StringBuilder tempText = new StringBuilder();
+
         ZipFile book = null;
+
+        List fileHeaders = null;
 
         try {
             book = new ZipFile(bookFile);
+
+            fileHeaders = book.getFileHeaders();
+
+            for (Object o : fileHeaders) {
+                FileHeader fileHeader = (FileHeader) o;
+
+                // Chapters in epub book is XHTML files
+                if (fileHeader.getFileName().contains(".xhtml")) {
+                    // Get chapter text
+                    InputStream chapterIn = book.getInputStream(fileHeader);
+                    tempText.append(getChapterText(chapterIn));
+                }
+            }
         } catch (ZipException e) {
-            e.printStackTrace();
+            logger.error("Book \"{}\" is corrupted", bookFile.getName(), e);
+
+            return bookText;
         }
+
+        bookText = tempText.toString();
 
         return bookText;
     }
 
+    /**
+     * Returns {@code String} with text for chapter from {@code chapterIn}
+     * stream obtained from zip (epub) file.
+     *
+     * @param chapterIn {@code InputStream} for chapter in epub book.
+     *
+     * @return {@code String} with chapter text.
+     */
+    private String getChapterText(InputStream chapterIn) {
+        String chapterText = "";
+
+        Document chapterDoc = null;
+
+        SAXBuilder builder = new SAXBuilder();
+
+        try {
+            chapterDoc = builder.build(chapterIn);
+        } catch (JDOMException e) {
+            logger.error("Can not read chapter", e);
+
+            return chapterText;
+        } catch (IOException e) {
+            logger.error("Can not read chapter", e);
+
+            return chapterText;
+        }
+
+        // Document root and namespace
+        Element root = chapterDoc.getRootElement();
+        Namespace ns = root.getNamespace();
+
+        // Get element with chapter text
+        Element body = root.getChild("body", ns);
+
+        XMLOutputter xmlOutputter = new XMLOutputter();
+
+        // Output chapter text to String
+        try (StringWriter stringOut = new StringWriter()) {
+            xmlOutputter.output(body, stringOut);
+            chapterText = stringOut.toString();
+        } catch (IOException e) {
+            logger.error("Can not output chapter text", e);
+        }
+
+        return chapterText;
+    }
 }

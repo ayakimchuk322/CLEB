fc4b91abc3fcd3a79a35fe7b8929d16117b4fc5a
Tol182 <toldark182@gmail.com>
2016-08-31 17:55:50 +0300

    minor refactoring

--- src/main/java/cleb/security/AuthStarter.java
+++ src/main/java/cleb/security/AuthStarter.java
@@ -2,16 +2,16 @@ package cleb.security;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.config.IniSecurityManagerFactory;
-import org.apache.shiro.util.Factory;
 import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.util.Factory;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 /**
- * This utility class initializes <a href="http://shiro.apache.org/">shiro</a>
- * at server start-up.
+ * This utility servlet class initializes
+ * <a href="http://shiro.apache.org/">Apache Shiro</a> at server start-up.
  */
 public class AuthStarter extends HttpServlet {
 
